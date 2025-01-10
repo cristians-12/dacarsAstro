c98edca1e08086d4b8b179fb29bfc6b95aec60bc
cristians-12 <cristian.sanchez.casd@gmail.com>
2025-01-10 00:03:26 -0500

    second commit

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,10 +1,16 @@
-import './Hero.css';
+import "./Hero.css";
 
-export default function Hero(){
-    return(
-        <div className="hero">
-            <h1>Welcome to Our Website!</h1>
-            <p>This is a simple hero component.</p>
-        </div>
-    )
+export default function Hero() {
+  return (
+    <section className="hero">
+      <article>
+        <h1>Bienvenido a nuestro sitio</h1>
+        <p>This is a simple hero component.</p>
+      </article>
+
+      <figure>
+        <img src="/images/img.png" alt="" />
+      </figure>
+    </section>
+  );
 }
